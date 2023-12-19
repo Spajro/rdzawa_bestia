883098df0966c7476aed287968b28f17476a5b10
Mateusz Gieroba <Mateusin26@gmail.com>
2023-12-19 11:34:47 +0100

    Add basic piece square tables

--- rust/src/evaluation.rs
+++ rust/src/evaluation.rs
@@ -1,4 +1,105 @@
-use shakmaty::{Chess, Position};
+use shakmaty::{Chess, Position, Square, File, Rank, Board};
+
+pub const KING_SQUARE_TABLE: [i32; 64] =
+[
+    -70, -70, -70, -70, -70, -70, -70, -70,
+    -70, -70, -70, -70, -70, -70, -70, -70,
+    -70, -70, -70, -70, -70, -70, -70, -70,
+    -70, -70, -70, -70, -70, -70, -70, -70,
+    -70, -70, -70, -70, -70, -70, -70, -70,
+    -60, -60, -60, -60, -60, -60, -60, -60,
+      3,   1,  -5, -15, -15,  -5,   1,   3,
+      5,  20,  10,   0,   5,   0,  40,  20,
+];
+
+pub const QUEEN_SQUARE_TABLE: [i32; 64] =
+[
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+];
+
+pub const ROOK_SQUARE_TABLE: [i32; 64] =
+[
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+];
+
+pub const BISHOP_SQUARE_TABLE: [i32; 64] =
+[
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+];
+
+pub const KNIGHT_SQUARE_TABLE: [i32; 64] =
+[
+     -60, -60, -30, -30, -30, -30, -60,  -60,
+     -40, -40, -20, -20, -20, -20, -40,  -40,
+     -20,  60,  40,  60,  60, 100,  50,   20,
+      -5,  10,  10,  40,  20,  20,  10,   10,
+     -10,   5,  10,  10,  20,  10,  20,   -8,
+     -15, -10,  12,  10,  10,  12, -10,  -15,
+     -20, -50, -10,   0,  0,  -10, -50,  -20,
+    -100, -20, -30, -40, -40, -30, -20, -100,
+];
+
+pub const PAWN_SQUARE_TABLE: [i32; 64] =
+[
+     0,  0,  0,  0,  0,  0,  0,  0,
+    20, 25, 30, 35, 35, 30, 25, 20,
+    15, 20, 25, 30, 30, 25, 20, 15,
+    10, 15, 20, 25, 25, 20, 15, 10,
+     5, 10, 15, 30, 30, 15, 10,  5,
+     3,  5, 10, 15, 15, 10,  5,  3,
+     0,  0,  0,  0,  0,  0,  0,  0,
+     0,  0,  0,  0,  0,  0,  0,  0,
+];
+
+
+pub fn get_sq_val(sq: Square, square_table: [i32; 64]) -> i32 {
+    let index = sq.file().distance(File::H) + 8 * sq.rank().distance(Rank::Eighth);
+    square_table[index as usize]
+}
+
+pub fn get_position_cummulative_value(board: &Board) -> f32 {
+    let king_pos_val = board.white().intersect(board.kings())
+        .into_iter()
+        .fold(0, |acc, sq| acc + get_sq_val(sq, KING_SQUARE_TABLE)) as f32;
+    let queen_pos_val = board.white().intersect(board.queens())
+        .into_iter()
+        .fold(0, |acc, sq| acc + get_sq_val(sq, QUEEN_SQUARE_TABLE)) as f32;
+    let rooks_pos_val = board.white().intersect(board.rooks())
+        .into_iter()
+        .fold(0, |acc, sq| acc + get_sq_val(sq, ROOK_SQUARE_TABLE)) as f32;
+    let bishops_pos_val = board.white().intersect(board.bishops())
+        .into_iter()
+        .fold(0, |acc, sq| acc + get_sq_val(sq, BISHOP_SQUARE_TABLE)) as f32;
+    let knights_pos_val = board.white().intersect(board.knights())
+        .into_iter()
+        .fold(0, |acc, sq| acc + get_sq_val(sq, KNIGHT_SQUARE_TABLE)) as f32;
+    let pawns_pos_val = board.white().intersect(board.pawns())
+        .into_iter()
+        .fold(0, |acc, sq| acc + get_sq_val(sq, PAWN_SQUARE_TABLE)) as f32;
+
+    (king_pos_val + queen_pos_val + rooks_pos_val + bishops_pos_val + knights_pos_val + pawns_pos_val) / 100.0
+}
 
 pub fn eval(chess: &Chess) -> f32 {
     let board = chess.board();
@@ -13,6 +114,7 @@ pub fn eval(chess: &Chess) -> f32 {
             0.0
         };
     }
+
     let white_value = board.white().intersect(board.pawns()).count() +
         3 * board.white().intersect(board.knights()).count() +
         3 * board.white().intersect(board.bishops()).count() +
@@ -23,7 +125,8 @@ pub fn eval(chess: &Chess) -> f32 {
         3 * board.black().intersect(board.bishops()).count() +
         5 * board.black().intersect(board.rooks()).count() +
         9 * board.black().intersect(board.queens()).count();
-    (white_value as i32 - black_value as i32) as f32
+
+    white_value as f32 - black_value as f32 + get_position_cummulative_value(board) as f32
 }
 
 
@@ -32,10 +135,12 @@ mod eval_tests {
     use shakmaty::{Chess, Move, Position, Role, Square};
     use crate::evaluation::eval;
 
+    pub const CLEAN_BOARD_EVALUATION: f32 = -0.4;
+
     #[test]
     fn start_board_test() {
         let chess = Chess::new();
-        assert_eq!(0.0, eval(&chess))
+        assert_eq!(CLEAN_BOARD_EVALUATION, eval(&chess))
     }
 
     #[test]
@@ -62,7 +167,7 @@ mod eval_tests {
             to: Square::D5,
             promotion: None,
         }).unwrap();
-        assert_eq!(1.0, eval(&chess3))
+        assert_eq!(1.25 + CLEAN_BOARD_EVALUATION, eval(&chess3))
     }
 
     #[test]
@@ -96,6 +201,6 @@ mod eval_tests {
             to: Square::D5,
             promotion: None,
         }).unwrap();
-        assert_eq!(0.0, eval(&chess4))
+        assert_eq!(CLEAN_BOARD_EVALUATION, eval(&chess4))
     }
 }
