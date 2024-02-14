7459461235995fcb344cf3f915287bbcc7d8e454
Spajro <rafal.spyra@onet.pl>
2024-02-14 01:12:16 +0100

    simplify structure of opening book update

--- src/features/opening_book.rs
+++ src/features/opening_book.rs
@@ -32,18 +32,26 @@ impl OpeningBook {
     }
 
     pub fn update(self, mv: String) -> OpeningBook {
-        return if self.node.clone().is_some() && self.node.clone().unwrap().has_key(mv.as_str()) {
-            send_info("Move in book:".to_string() + &*mv);
-            let nxt = self.node.unwrap()[mv.as_str()].clone();
-            OpeningBook {
-                node: Some(nxt)
-            }
-        } else {
-            send_info("Move not in book:".to_string() + &*mv);
-            OpeningBook {
+        if self.node.is_none() {
+            send_info("Book empty: ".to_string() + &*mv);
+            return OpeningBook {
                 node: None
-            }
+            };
+        }
+
+        let node = self.node.unwrap();
+        if !node.has_key(mv.as_str()) {
+            send_info("Move not in book: ".to_string() + &*mv);
+            return OpeningBook {
+                node: None
+            };
         };
+
+        send_info("Move in book:".to_string() + &*mv);
+        let nxt = node[mv.as_str()].clone();
+        OpeningBook {
+            node: Some(nxt)
+        }
     }
 
     pub fn try_get_best(self, pos: &Chess) -> QueryResult {
@@ -54,8 +62,9 @@ impl OpeningBook {
                 book: OpeningBook {
                     node: None
                 },
-            }
+            };
         }
+
         let node = self.node.unwrap();
         if !node.has_key("best") {
             send_info("No move found".to_string());
@@ -64,11 +73,12 @@ impl OpeningBook {
                 book: OpeningBook {
                     node: None
                 },
-            }
+            };
         }
+
         let mv = node["best"].as_str().unwrap();
         let nxt = node[mv].clone();
-        send_info("Move from book:".to_string() + mv);
+        send_info("Move from book: ".to_string() + mv);
         let mov = Uci::from_str(mv).unwrap().to_move(pos).unwrap();
         QueryResult {
             mv: Some(mov),
