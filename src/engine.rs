e8668db35f2d2867c9f82d3752c8779a349c7355
Dominik Kowalczyk <xkowal99@gmail.com>
2024-02-18 17:22:30 +0100

    Switch from shakmaty to chess.

--- src/engine.rs
+++ src/engine.rs
@@ -1,49 +1,49 @@
-use shakmaty::{Chess, Move, Position};
+use chess::{Board, ChessMove};
 use rand::seq::SliceRandom;
 use crate::io::output::send_move;
 
 pub trait Engine {
     fn start(&mut self, time: u64);
     fn stop(&mut self);
-    fn update(&mut self, mv: Move);
+    fn update(&mut self, mv: ChessMove);
     fn restart(&mut self);
-    fn get_status(&self) -> Chess;
+    fn get_status(&self) -> Board;
 }
 
 pub struct RandomEngine {
-    pub pos: Chess,
+    pub pos: Board,
 }
 
 impl Engine for RandomEngine {
     fn start(&mut self, _time: u64) {
-        send_move(self.next_move())
+        // send_move(self.next_move())
     }
 
     fn stop(&mut self) {
-        send_move(self.next_move())
+        // send_move(self.next_move())
     }
 
-    fn update(&mut self, mv: Move) {
-        self.pos.play_unchecked(&mv);
+    fn update(&mut self, mv: ChessMove) {
+        self.pos = self.pos.make_move_new(mv);
     }
 
     fn restart(&mut self) {
-        self.pos = Chess::default();
+        self.pos = Board::default();
     }
 
-    fn get_status(&self) -> Chess {
+    fn get_status(&self) -> Board {
         self.pos.clone()
     }
 }
 
-impl RandomEngine {
-    fn next_move(&mut self) -> Move {
-        let moves = self.pos
-            .legal_moves();
-        let mv = moves
-            .choose(&mut rand::thread_rng())
-            .unwrap();
-        self.pos.play_unchecked(mv);
-        mv.clone()
-    }
-}
+// impl RandomEngine {
+//     fn next_move(&mut self) -> ChessMove {
+//         let moves = self.pos
+//             .legal_moves();
+//         let mv = moves
+//             .choose(&mut rand::thread_rng())
+//             .unwrap();
+//         self.pos.play_unchecked(mv);
+//         mv.clone()
+//     }
+// }
--- src/features/killer_moves.rs
+++ src/features/killer_moves.rs
@@ -1,9 +1,11 @@
+use std::f32::consts::SQRT_2;
+
 use arrayvec::ArrayVec;
-use shakmaty::Move;
+use chess::{ChessMove, Square};
 
 #[derive(Clone)]
 pub struct KillerMoves<const MAX_SIZE: usize> {
-    pub(crate) moves: ArrayVec<Move, MAX_SIZE>,
+    pub(crate) moves: ArrayVec<ChessMove, MAX_SIZE>,
     pub(crate) size: usize,
 }
 
@@ -11,17 +13,17 @@ impl<const MAX_SIZE: usize> KillerMoves<MAX_SIZE> {
     pub fn new() -> Self {
         let mut moves = ArrayVec::<_, { MAX_SIZE }>::new();
 
-        (0..MAX_SIZE).into_iter().map(|_| Move::Put {
-            role: shakmaty::Role::Pawn,
-            to: shakmaty::Square::E5,
-        }).for_each(|put| moves.push(put));
+        (0..MAX_SIZE)
+            .into_iter()
+            .map(|_| ChessMove::new(Square::A1, Square::A1, None))
+            .for_each(|put| moves.push(put));
 
         KillerMoves::<MAX_SIZE> {
             moves: moves,
             size: 0,
         }
     }
-    pub fn add(&mut self, mv: Move) {
+    pub fn add(&mut self, mv: ChessMove) {
         self.moves.rotate_right(1);
         self.size = std::cmp::min(self.size + 1, MAX_SIZE);
         self.moves[0] = mv;
