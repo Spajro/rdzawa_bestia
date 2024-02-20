8d0113bbd8583db27c4ea5467596bf4d2e4d8ffe
Dominik Kowalczyk <xkowal99@gmail.com>
2024-02-20 20:00:23 +0100

    Update opening_book.

--- src/features/opening_book.rs
+++ src/features/opening_book.rs
@@ -1,10 +1,9 @@
+use crate::io::output::send_info;
+use chess::ChessMove;
+use json::JsonValue;
 use std::fs;
 use std::path::Path;
 use std::str::FromStr;
-use json::JsonValue;
-use shakmaty::{Chess, Move};
-use shakmaty::uci::Uci;
-use crate::io::output::send_info;
 
 #[derive(Clone)]
 pub struct OpeningBook {
@@ -12,7 +11,7 @@ pub struct OpeningBook {
 }
 
 pub struct QueryResult {
-    pub mv: Option<Move>,
+    pub mv: Option<ChessMove>,
     pub book: OpeningBook,
 }
 
@@ -21,47 +20,35 @@ impl OpeningBook {
         return if Path::new("book.json").exists() {
             let json = fs::read_to_string("book.json").unwrap();
             let book = json::parse(&*json).unwrap();
-            OpeningBook {
-                node: Some(book)
-            }
+            OpeningBook { node: Some(book) }
         } else {
-            OpeningBook {
-                node: None,
-            }
+            OpeningBook { node: None }
         };
     }
 
     pub fn update(self, mv: String) -> OpeningBook {
         if self.node.is_none() {
             send_info("Book empty: ".to_string() + &*mv);
-            return OpeningBook {
-                node: None
-            };
+            return OpeningBook { node: None };
         }
 
         let node = self.node.unwrap();
         if !node.has_key(mv.as_str()) {
             send_info("Move not in book: ".to_string() + &*mv);
-            return OpeningBook {
-                node: None
-            };
+            return OpeningBook { node: None };
         };
 
         send_info("Move in book:".to_string() + &*mv);
         let nxt = node[mv.as_str()].clone();
-        OpeningBook {
-            node: Some(nxt)
-        }
+        OpeningBook { node: Some(nxt) }
     }
 
-    pub fn try_get_best(self, pos: &Chess) -> QueryResult {
+    pub fn try_get_best(self) -> QueryResult {
         if self.node.is_none() {
             send_info("No move found".to_string());
             return QueryResult {
                 mv: None,
-                book: OpeningBook {
-                    node: None
-                },
+                book: OpeningBook { node: None },
             };
         }
 
@@ -70,21 +57,17 @@ impl OpeningBook {
             send_info("No move found".to_string());
             return QueryResult {
                 mv: None,
-                book: OpeningBook {
-                    node: None
-                },
+                book: OpeningBook { node: None },
             };
         }
 
         let mv = node["best"].as_str().unwrap();
         let nxt = node[mv].clone();
         send_info("Move from book: ".to_string() + mv);
-        let mov = Uci::from_str(mv).unwrap().to_move(pos).unwrap();
+        let mov = ChessMove::from_str(mv).unwrap();
         QueryResult {
             mv: Some(mov),
-            book: OpeningBook {
-                node: Some(nxt),
-            },
+            book: OpeningBook { node: Some(nxt) },
         }
     }
 }
