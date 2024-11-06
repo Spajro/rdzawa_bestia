89f852ee5f45b101f1affdab62df95d173bc4018
Spajro <rafal.spyra@onet.pl>
2024-11-06 22:11:50 +0100

    add test environment setup

---
+++ tests/setup.sh
@@ -0,0 +1,17 @@
+if [ ! -e chess_engine_evaluator ]; then
+  git clone https://github.com/Spajro/chess_engine_evaluator
+fi
+
+if [ "$(uname -s)" == "Linux"  ] && [ ! -e stockfish ]; then
+  wget https://github.com/official-stockfish/Stockfish/releases/latest/download/stockfish-ubuntu-x86-64-avx2.tar
+  tar -xf stockfish-ubuntu-x86-64-avx2.tar
+  rm stockfish-ubuntu-x86-64-avx2.tar
+fi
+
+python3 -m pip install zstandard
+python3 -m pip install stockfish
+python3 -m pip install chess
+
+python3 chess_engine_evaluator/setup.py stockfish stockfish/stockfish-ubuntu-x86-64-avx2
+python3 chess_engine_evaluator/setup.py uci rdzawa_bestia ../target/debug/rdzawa_bestia.exe
+python3 chess_engine_evaluator/setup.py puzzles
